8b89a916cf03e6900cb75cb44bcf2ac4b90f6fe8
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-03-27 21:50:46 +0530

    learn python basic variable and data type

---
+++ Python-Basic/VarDataType.py
@@ -0,0 +1,17 @@
+# Integer
+x = 10  
+# Float
+y = 3.14  
+# String
+name = "Swagat"  
+# Boolean
+is_data_scientist = True  
+
+print("Integer:", x)
+print("Float:", y)
+print("String:", name)
+print("Boolean:", is_data_scientist)
+print("Data Type of x:", type(x))
+print("Data Type of y:", type(y))
+print("Data Type of name:", type(name))
+print("Data Type of is_data_scientist:", type(is_data_scientist))
